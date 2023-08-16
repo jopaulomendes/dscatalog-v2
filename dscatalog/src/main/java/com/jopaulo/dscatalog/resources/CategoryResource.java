fcf30912e9081355fd6b6094d6b1909c20aa6f5b
João Paulo <jp_cbc@hotmail.com>
2023-08-15 23:51:51 -0300

    Banco de dados H2 e camadas

--- dscatalog/src/main/java/com/jopaulo/dscatalog/resources/CategoryResource.java
+++ dscatalog/src/main/java/com/jopaulo/dscatalog/resources/CategoryResource.java
@@ -1,25 +1,26 @@
 package com.jopaulo.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jopaulo.dscatalog.entities.Category;
+import com.jopaulo.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Livros"));
-		list.add(new Category(2L, "Eletrônicos"));
-		list.add(new Category(3L, "Móveis"));
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }

36caf4812b3eaaf9d22de6322a363daaa8fc1e00
João Paulo <jp_cbc@hotmail.com>
2023-08-18 00:34:22 -0300

    Refatoração no Pageable

--- dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/ProductRepository.java
+++ dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/ProductRepository.java
@@ -4,7 +4,6 @@ package com.jopaulo.dscatalog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jopaulo.dscatalog.entities.Category;
 import com.jopaulo.dscatalog.entities.Product;
 
 @Repository

b22d076654ccaf52b00a16d773f048720ad3ba06
Joao <joao.mata@sicoob.com.br>
2023-09-13 16:20:18 -0300

    Resolvendo Warnes

--- dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/CategoryRepository.java
+++ dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/CategoryRepository.java
@@ -2,11 +2,9 @@ package com.jopaulo.dscatalog.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.jopaulo.dscatalog.entities.Category;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/RoleRepository.java
+++ dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/RoleRepository.java
@@ -2,12 +2,9 @@ package com.jopaulo.dscatalog.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.jopaulo.dscatalog.entities.Role;
-import com.jopaulo.dscatalog.entities.User;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }

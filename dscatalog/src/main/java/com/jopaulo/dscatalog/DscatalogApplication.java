c3ef14601c85e541ef5cc76d5ec4a4e561cbf102
João Paulo <jp_cbc@hotmail.com>
2023-09-14 00:00:13 -0300

    Elaborando a consulta SQL

--- dscatalog/src/main/java/com/jopaulo/dscatalog/DscatalogApplication.java
+++ dscatalog/src/main/java/com/jopaulo/dscatalog/DscatalogApplication.java
@@ -9,5 +9,12 @@ public class DscatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
+	
+//	SELECT DISTINCT TB_PRODUCT.ID, TB_PRODUCT.NAME
+//	FROM TB_PRODUCT 
+//	INNER JOIN TB_PRODUCT_CATEGORY ON TB_PRODUCT.ID = TB_PRODUCT_CATEGORY.PRODUCT_ID
+//	WHERE TB_PRODUCT_CATEGORY.CATEGORY_ID IN (1,3)
+//	AND LOWER(TB_PRODUCT.NAME) LIKE '%ma%'
+//	ORDER BY TB_PRODUCT.NAME
 
 }

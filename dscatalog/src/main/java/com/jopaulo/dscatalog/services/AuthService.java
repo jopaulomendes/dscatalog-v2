a112ab06b9595f345a000306261cd8325dd362ff
Joao <joao.mata@sicoob.com.br>
2023-09-18 11:35:09 -0300

    Recuperação de senha

---
+++ dscatalog/src/main/java/com/jopaulo/dscatalog/services/AuthService.java
@@ -0,0 +1,14 @@
+package com.jopaulo.dscatalog.services;
+
+import org.springframework.stereotype.Service;
+
+import com.jopaulo.dscatalog.dto.EmailDTO;
+
+@Service
+public class AuthService {
+
+	public void createRecoverToken(EmailDTO body) {
+		
+	}
+
+}

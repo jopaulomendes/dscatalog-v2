bb60facc67d57d27faec6cd310ddea7dbbae9d62
Joao <joao.mata@sicoob.com.br>
2023-09-21 11:32:52 -0300

    Melhorias no método autehnticade()

--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -17,43 +17,44 @@ import com.devsuperior.dscommerce.entities.Role;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 import com.devsuperior.dscommerce.repositories.UserRepository;
+import com.devsuperior.dscommerce.util.CustomUserUtil;
 
 @Service
 public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
+	@Autowired
+	private CustomUserUtil customUserUtil;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
 		if (result.size() == 0) {
 			throw new UsernameNotFoundException("Email not found");
 		}
-		
+
 		User user = new User();
 		user.setEmail(result.get(0).getUsername());
 		user.setPassword(result.get(0).getPassword());
 		for (UserDetailsProjection projection : result) {
 			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
-		
+
 		return user;
 	}
-	
+
 	protected User authenticated() {
 		try {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-			String username = jwtPrincipal.getClaim("username");
+			String username = customUserUtil.getLoggedUserName();
 			return repository.findByEmail(username).get();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			throw new UsernameNotFoundException("Invalid user");
 		}
 	}
-	
+
 	@Transactional(readOnly = true)
 	public UserDTO getMe() {
 		User entity = authenticated();

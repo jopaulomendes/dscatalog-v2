6d651e4a081709aa56c5518765db7cb9bde824e9
João Paulo <jp_cbc@hotmail.com>
2023-09-29 00:37:56 -0300

    Teste de busca de produto por nome

--- dscommerce-restassure/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ dscommerce-restassure/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -4,17 +4,20 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.http.HttpStatusCode;
 
 public class ProductControllerRA {
 
 	private Long existProductId, nonExistingProductId;
+	private String productName;
 	
 	@BeforeEach
 	void setup() {
 		baseURI = "http://localhost:8080";
+		
+		productName = "Macbook";
 	}
 	
 	@Test
@@ -22,14 +25,44 @@ public class ProductControllerRA {
 		existProductId = 2L;
 		
 		given()
-			.get("/product/{id}", 2)
+			.get("/products/{id}", existProductId)
 		.then()
 			.statusCode(200)
-			.body("id", is(existProductId))
+			.body("id", is(2))
 			.body("name", equalTo("Smart TV"))
 			.body("price", is(2190.0F))
 			.body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
 			.body("categories.id", hasItems(2, 3))
 			.body("categories.name", hasItems("Eletrônicos", "Computadores"));
+	}
+	
+	@Test
+	public void findAllShoultReturnPageProductsWhenProductNameIsEmpty() {
+		given()
+			.get("/products?page=0")
+		.then()
+			.statusCode(200)
+			.body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+	}
+	
+	@Test
+	public void findAllShoultReturnPageProductsWhenProductNameIsNotEmpty() {
+		given()
+			.get("/products?name={productName}", productName)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(3))
+			.body("content.name[0]", equalTo("Macbook Pro"))
+			.body("content.price[0]", is(1250.0F))
+			.body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+	}
+	
+	@Test
+	public void findAllShoultReturnPageProductsWhithPriceGreaterThan200() {
+		given()
+			.get("/products?size=25")
+		.then()
+			.statusCode(200)
+			.body("content.findAll {it.price > 2000}.name", hasItems("Smart TV", "PC Gamer Hera"));
 	}
 }

121fecdfb91176e7df4f26ea22d1d4b8290cd464
João Paulo <jp_cbc@hotmail.com>
2023-09-29 01:17:41 -0300

    Teste inserindo produto com dados validos

--- dscommerce-restassure/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ dscommerce-restassure/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -4,20 +4,53 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.http.HttpStatusCode;
+
+import io.restassured.http.ContentType;
 
 public class ProductControllerRA {
 
 	private Long existProductId, nonExistingProductId;
 	private String productName;
 	
+	private Map<String, Object> postProductInstance;
+	List<Map<String, Object>> categories;
+	
+	Map<String, Object> category1;
+	Map<String, Object> category2;
+	
 	@BeforeEach
 	void setup() {
 		baseURI = "http://localhost:8080";
 		
 		productName = "Macbook";
+		
+		postProductInstance = new HashMap<>();
+		postProductInstance.put("name", "Meu produto");
+		postProductInstance.put("description", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ad, adipisci illum ipsam velit et odit eaque reprehenderit ex maxime delectus dolore labore, quisquam quae tempora natus esse aliquam veniam doloremque quam minima culpa alias maiores commodi. Perferendis enim");
+		postProductInstance.put("imgUrl", "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+		postProductInstance.put("price", 50.0);
+		
+		categories = new ArrayList<>();
+		
+		category1 = new HashMap<>();
+		category1.put("id", 2);
+		
+		category2 = new HashMap<>();
+		category2.put("id", 3);
+		
+		categories.add(category1);
+		categories.add(category2);
+		
+		postProductInstance.put("categories", categories);
 	}
 	
 	@Test
@@ -64,5 +97,26 @@ public class ProductControllerRA {
 		.then()
 			.statusCode(200)
 			.body("content.findAll {it.price > 2000}.name", hasItems("Smart TV", "PC Gamer Hera"));
+	}
+	
+	@Test
+	public void insertShoultReturnProductCreatedWhenAdminLogged() {
+		JSONObject newProduct = new JSONObject(postProductInstance);
+		String adminToken = "";
+		
+		given()
+			.header("Content-Type", "application/json")
+			.header("Authorization", "Beare " + adminToken )
+			.body(newProduct)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/products")
+		.then()
+			.statusCode(201)
+			.body("name", equalTo("Meu produto"))
+			.body("price", is(50F))
+			.body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"))
+			.body("categories", hasItems(2, 3));
 	}
 }
